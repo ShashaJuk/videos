3000506c8b023a24d1fc62d69c64fcf282a00497
JuchokJuk <juk.juchok@yandex.ru>
2023-04-04 19:24:47 +0300

    .

--- main.js
+++ main.js
@@ -2,17 +2,47 @@ const loader = document.getElementById("loader");
 const avatar1 = document.getElementById("avatar1");
 const avatar2 = document.getElementById("avatar2");
 const avatar3 = document.getElementById("avatar3");
+const progressSpan = document.getElementById('progress');
+
+const percentageLoad = new PercentageLoad();
+const result = await percentageLoad.load(
+  [
+    "https://shashajuk.github.io/videos/0.mp4",
+    "https://shashajuk.github.io/videos/1.mp4",
+    "https://shashajuk.github.io/videos/2.mp4",
+    "https://shashajuk.github.io/videos/3.mp4",
+  ],
+  (progress) => {
+    console.log("progress:", progress);
+    progressSpan.innerHTML = `${Math.round(progress)}% loaded`
+  }
+);
+
 
 const avatars = [avatar1, avatar2, avatar3];
 
 const videoPromises = [];
 
-for (const video of [loader, ...avatars]) {
-  video.load();
+function createObjectURL(file) {
+  if (window.webkitURL) {
+    return window.webkitURL.createObjectURL(file);
+  } else if (window.URL && window.URL.createObjectURL) {
+    return window.URL.createObjectURL(file);
+  } else {
+    console.warn("❌ no window.URL / window.webkitURL");
+    return null;
+  }
+}
+
+for (let i = 0; i < [loader, ...avatars].length; i++) {
+  [loader, ...avatars][i].src = createObjectURL(
+    new Blob([result[i]], { type: "video/mp4" })
+  );
+  [loader, ...avatars][i].load();
   videoPromises.push(
     new Promise((resolve) => {
-      video.oncanplaythrough = () => {
-        video.oncanplaythrough = null;
+      [loader, ...avatars][i].oncanplaythrough = () => {
+        [loader, ...avatars][i].oncanplaythrough = null;
         resolve();
       };
     })
@@ -29,7 +59,7 @@ Promise.all(videoPromises).then(() => {
   loader.onended = () => {
     loader.style.opacity = 0;
     avatar1.style.opacity = 1;
-    avatar1.style.filter = 'blur(0px) hue-rotate(0deg) contrast(1)';
+    avatar1.style.filter = "blur(0px) hue-rotate(0deg) contrast(1)";
     avatar1.play();
     avatar1.ontimeupdate = () => {
       avatar1.ontimeupdate = null;
@@ -63,47 +93,47 @@ Promise.all(videoPromises).then(() => {
     function swapVideos(prev, next) {
       // applyFillter();
       // setTimeout(() => {
-        prev.pause();
-        const time = prev.currentTime;
-        next.currentTime = time;
-        next.ontimeupdate = () => {
-          next.ontimeupdate = null;
-          prev.style.opacity = 0;
-          prev.style.filter = 'blur(32px) hue-rotate(180deg) contrast(10)';
-          next.style.opacity = 1;
-          next.style.filter = 'blur(0px) hue-rotate(0deg) contrast(1)';
-          next.play();
-        };
+      prev.pause();
+      const time = prev.currentTime;
+      next.currentTime = time;
+      next.ontimeupdate = () => {
+        next.ontimeupdate = null;
+        prev.style.opacity = 0;
+        prev.style.filter = "blur(32px) hue-rotate(180deg) contrast(10)";
+        next.style.opacity = 1;
+        next.style.filter = "blur(0px) hue-rotate(0deg) contrast(1)";
+        next.play();
+      };
       // }, 1500);
     }
   };
 });
 
-function applyFillter() {
-  const element = document.createElement("div");
-  element.innerHTML = `
-  <svg class="filter" xmlns="http://www.w3.org/2000/svg">
-    <filter id="displacementFilter">
-      <feTurbulence type="turbulence" baseFrequency="0" numOctaves="1" result="turbulence">
-        <animate
-        attributeName="baseFrequency"
-        values="0;0.05;0"
-        dur="5s"
-        repeatCount="indefinite" />
-      </feTurbulence>
-      <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G">
-        <animate
-        attributeName="scale"
-        values="0;50;0"
-        dur="5s"
-        repeatCount="indefinite" />
-      </feDisplacementMap>
-    </filter>
-  </svg>
-  `;
-  const fillter = element.firstElementChild;
-  document.documentElement.appendChild(fillter);
-  setTimeout(() => {
-    fillter.remove();
-  }, 5000);
-}
+// function applyFillter() {
+//   const element = document.createElement("div");
+//   element.innerHTML = `
+//   <svg class="filter" xmlns="http://www.w3.org/2000/svg">
+//     <filter id="displacementFilter">
+//       <feTurbulence type="turbulence" baseFrequency="0" numOctaves="1" result="turbulence">
+//         <animate
+//         attributeName="baseFrequency"
+//         values="0;0.05;0"
+//         dur="5s"
+//         repeatCount="indefinite" />
+//       </feTurbulence>
+//       <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G">
+//         <animate
+//         attributeName="scale"
+//         values="0;50;0"
+//         dur="5s"
+//         repeatCount="indefinite" />
+//       </feDisplacementMap>
+//     </filter>
+//   </svg>
+//   `;
+//   const fillter = element.firstElementChild;
+//   document.documentElement.appendChild(fillter);
+//   setTimeout(() => {
+//     fillter.remove();
+//   }, 5000);
+// }
