67c2f6fe9a765a10ac2d3b920e129813608f6de2
JuchokJuk <juk.juchok@yandex.ru>
2023-04-06 12:18:25 +0300

    .

--- main.js
+++ main.js
@@ -2,22 +2,19 @@ const loader = document.getElementById("loader");
 const avatar1 = document.getElementById("avatar1");
 const avatar2 = document.getElementById("avatar2");
 const avatar3 = document.getElementById("avatar3");
-const progressSpan = document.getElementById('progress');
+const progressSpan = document.getElementById("progress");
+const pixelateFillter = document.getElementById("pixelate");
+
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const sources = isSafari
+  ? ["video/0.mov", "video/1.mov", "video/2.mov", "video/3.mov"]
+  : ["video/0.webm", "video/1.webm", "video/2.webm", "video/3.webm"];
 
 const percentageLoad = new PercentageLoad();
-const result = await percentageLoad.load(
-  [
-    "https://shashajuk.github.io/videos/0.mov",
-    "https://shashajuk.github.io/videos/1.mov",
-    "https://shashajuk.github.io/videos/2.mov",
-    "https://shashajuk.github.io/videos/3.mov",
-  ],
-  (progress) => {
-    console.log("progress:", progress);
-    progressSpan.innerHTML = `${Math.round(progress)}% loaded`
-  }
-);
-
+const result = await percentageLoad.load(sources, (progress) => {
+  console.log("progress:", progress);
+  progressSpan.innerHTML = `${Math.round(progress)}% loaded`;
+});
 
 const avatars = [avatar1, avatar2, avatar3];
 
@@ -36,7 +33,7 @@ function createObjectURL(file) {
 
 for (let i = 0; i < [loader, ...avatars].length; i++) {
   [loader, ...avatars][i].src = createObjectURL(
-    new Blob([result[i]], { type: 'video/mp4; codecs="hvc1"' })
+    new Blob([result[i]], { type: "video/webm" })
   );
   [loader, ...avatars][i].load();
   videoPromises.push(
@@ -59,14 +56,14 @@ Promise.all(videoPromises).then(() => {
   loader.onended = () => {
     loader.style.opacity = 0;
     avatar1.style.opacity = 1;
-    avatar1.style.filter = "blur(0px) hue-rotate(0deg) contrast(1)";
+    // avatar1.style.filter = "blur(0px) hue-rotate(0deg) contrast(1)";
     avatar1.play();
-    avatar1.ontimeupdate = () => {
-      avatar1.ontimeupdate = null;
-      for (const avatar of avatars) {
-        avatar.style.transition = "opacity 2s, filter 2s";
-      }
-    };
+    // avatar1.ontimeupdate = () => {
+      // avatar1.ontimeupdate = null;
+      // for (const avatar of avatars) {
+      // avatar.style.transition = "opacity 2s, filter 2s";
+      // }
+    // };
     buttonPrev.disabled = false;
     buttonNext.disabled = false;
 
@@ -91,49 +88,72 @@ Promise.all(videoPromises).then(() => {
     };
 
     function swapVideos(prev, next) {
-      // applyFillter();
-      // setTimeout(() => {
-      prev.pause();
-      const time = prev.currentTime;
-      next.currentTime = time;
-      next.ontimeupdate = () => {
-        next.ontimeupdate = null;
-        prev.style.opacity = 0;
-        prev.style.filter = "blur(32px) hue-rotate(180deg) contrast(10)";
-        next.style.opacity = 1;
-        next.style.filter = "blur(0px) hue-rotate(0deg) contrast(1)";
-        next.play();
-      };
-      // }, 1500);
+      let tick = 0;
+      let level = 0;
+      const step = 0.2;
+      let prevLevel = 0;
+
+      function increase() {
+        tick++;
+        level += step;
+        const roundedLevel = Math.round(level);
+        if (roundedLevel !== prevLevel) {
+          updateFillter(roundedLevel);
+        }
+        prevLevel = Math.round(level);
+        if (tick === 50) {
+          // prev.pause();
+          const time = prev.currentTime;
+          next.currentTime = time;
+          console.log("&");
+          next.ontimeupdate = () => {
+            //
+            prev.pause();
+            //
+            console.log("!");
+            next.ontimeupdate = null;
+            prev.style.opacity = 0;
+            next.style.opacity = 1;
+            next.play();
+            decrease();
+          };
+        }else if (tick < 100) {
+          requestAnimationFrame(increase);
+        }
+      }
+
+      function decrease() {
+        tick++;
+        level -= step;
+        const roundedLevel = Math.round(level);
+        if (roundedLevel !== prevLevel) {
+          updateFillter(roundedLevel);
+        }
+        prevLevel = Math.round(level);
+        if (tick < 100) {
+          requestAnimationFrame(decrease);
+        }
+      }
+
+      increase();
     }
   };
 });
 
-// function applyFillter() {
-//   const element = document.createElement("div");
-//   element.innerHTML = `
-//   <svg class="filter" xmlns="http://www.w3.org/2000/svg">
-//     <filter id="displacementFilter">
-//       <feTurbulence type="turbulence" baseFrequency="0" numOctaves="1" result="turbulence">
-//         <animate
-//         attributeName="baseFrequency"
-//         values="0;0.05;0"
-//         dur="5s"
-//         repeatCount="indefinite" />
-//       </feTurbulence>
-//       <feDisplacementMap in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G">
-//         <animate
-//         attributeName="scale"
-//         values="0;50;0"
-//         dur="5s"
-//         repeatCount="indefinite" />
-//       </feDisplacementMap>
-//     </filter>
-//   </svg>
-//   `;
-//   const fillter = element.firstElementChild;
-//   document.documentElement.appendChild(fillter);
-//   setTimeout(() => {
-//     fillter.remove();
-//   }, 5000);
-// }
+function updateFillter(level) {
+  console.log(level);
+  const radius = level;
+  const size = Math.max(1, level * 2);
+
+  if (level === 0) {
+    pixelate.innerHTML = "";
+  } else {
+    pixelate.innerHTML = `
+    <feFlood x="0" y="0" height="1" width="1"/>    
+    <feComposite width="${size}" height="${size}"/>
+    <feTile result="a"/>
+    <feComposite in="SourceGraphic" in2="a" operator="in"/>
+    <feMorphology operator="dilate" radius="${radius}">
+  `;
+  }
+}
